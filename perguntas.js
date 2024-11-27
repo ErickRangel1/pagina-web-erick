cfd6d34dfbfe671c454a617c1a9e86ea5cae307a
ErickRangel1 <00001093752579sp@al.educacao.sp.gov.br>
2024-11-27 07:45:27 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Em qual continente fica o deserto do Saara?',
+    'O deserto do Saara fica no continente africano.'
+)
+
+criaCartao(
+    'Conhecimentos Gerais',
+    'Qual é o animal mais lento do mundo?',
+    'O animal mais lento do mundo é o bicho-preguiça, que se move a uma velocidade média de apenas 0,03 km/h! 🦥'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual animal é conhecido por ter a língua mais longa em relação ao tamanho do corpo?',
+    'O camaleão é conhecido por ter a língua mais longa em relação ao tamanho do corpo. Sua língua pode ser até duas vezes o comprimento de seu corpo!'
+)
+
+criaCartao(
+    'Conhecimentos Gerais',
+    'Qual país é conhecido como a "terra do sol nascente"?',
+    'O Japão é conhecido como a "terra do sol nascente" devido à sua localização geográfica, onde o sol nasce primeiro em relação ao continente asiático.'
+)
